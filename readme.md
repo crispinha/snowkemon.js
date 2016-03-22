807cc76ca33b5ef47f746e090261c7e187901fc4
Crispin <crispin@developster.io>
2016-03-23 11:59:41 +1300

    Moved the other stuff elsewhere. This'll mess with version control/web servers/everything.

--- readme.md
+++
@@ -1,3 +0,0 @@
-Hey
-
-This is a thing.
--- rpg/test_funcion.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by crispin on 11/03/16.
- */
-function yermam(text) {
-	text = text || 'doot'
-	console.log(text);
-}
-yermam();
-yermam('gjhsyjgwejhs');
