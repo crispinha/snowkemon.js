110e5ddf61dbfc0ab2ee7c342987d0122e5817fb
Crispin Hitchings-Anstice <dvlpstrcrispin@users.noreply.github.com>
2016-03-24 18:16:25 +1300

    i hate fonts

--- game.js
+++ game.js
@@ -48,12 +48,12 @@ function create() {
 	run.inputEnabled = true;
 	//characters
 	var player = game.add.sprite(79, 183, 'good');
-	player.stats = {name: 'Pikasnow', health: 100, attack: 7, defense: 5, heal: 3, speed: 8};
+	player.stats = {name: 'Pikasnow', health: 100, maxHealth: 100, attack: 7, defense: 5, heal: 3, speed: 8};
 	var enemy = game.add.sprite(675, 25, 'bad');
-	enemy.stats = {name: 'Pr.Snowk', health: 100, attack: 4, defense: 8, heal: 6, speed: 5};
+	enemy.stats = {name: 'Pr.Snowk', health: 100, maxHealth: 100, attack: 4, defense: 8, heal: 6, speed: 5};
 	//hp
 	var playerHP = game.add.text(300, 375, player.stats.name + ' ' + hpList[10], {font: 'VT323',fontSize:'42px', fill:"#000"});
-	var enemyHP = game.add.text(250, 25, enemy.stats.name + ' ' + hpList[10], {font: 'VT323',fontSize:'42px', fill:"#000"});
+	var enemyHP = game.add.text(200, 25, enemy.stats.name + ' ' + hpList[10], {font: 'VT323',fontSize:'42px', fill:"#000"});
 	//delay
 	var delay = 20;
 	//introduction
@@ -62,7 +62,8 @@ function create() {
 	//button commands
 	attack.events.onInputDown.add(function(){doAttack(player, enemy, messageBox, function(){movePlayer(player)})});
 	heal.events.onInputDown.add(function(){doHeal(player, messageBox)}, this);
-	run.events.onInputDown.add(function(){updateText('It is literally impossible for you to \nrun away.\nThere is only one fight in this \nentire game.', messageBox)});
+	run.events.onInputDown.add(function(){calculateHealthBars(player, enemy, playerHP, enemyHP)});
+	//run.events.onInputDown.add(function(){updateText('It is literally impossible for you to \nrun away.\nThere is only one fight in this \nentire game.', messageBox)});
 	quit.events.onInputDown.add(function(){updateText('If you know how to close a window in \nJavascript, let me know.\n@dvlpstrcrispin', messageBox)}, this);
 	//debug stuff
 	appendDebug('debug activated');
@@ -73,14 +74,27 @@ function doAttack (self, target, messageBox, callback) {
 	icallback = null;
 	icallback = callback || function(){return null;};
 	var damage = Math.floor(getRandomFloat(0.7, 1.3) * self.stats.attack);
-	updateText(self.stats.name + ' attacks for ' + damage + ' damage.', messageBox, icallback);
+	target.stats.health = target.stats.health - damage;
+	updateText(self.stats.name + ' attacks for ' + damage + ' damage.\nThey are on ' + target.stats.health + ' health.', messageBox, icallback);
 }
 function doHeal (self, messageBox, callback) {
 	icallback = null;
 	icallback = callback || function(){return null;};
 	var heal = Math.floor(getRandomFloat(0.7, 1.3) * self.stats.heal);
+	self.stats.health = self.stats.health + heal;
 	updateText(self.stats.name + ' heals ' + heal + ' hp.', messageBox, icallback);
 }
+function calculateHealthBars(player, enemy, playerText, enemyText) {
+	playerHealthRounded = (player.stats.health / 10).toFixed(0);
+	enemyHealthRounded = (enemy.stats.health / 10).toFixed(0);
+	//if health is equal to or greater than max health, use full bar, else use rounded bar
+	if (player.stats.health >= player.stats.maxHealth) {playerText.text = player.stats.name + ' ' + hpList[10]}
+		else {playerText.text = player.stats.name + ' ' + hpList[playerHealthRounded]};
+	if (enemy.stats.health >= enemy.stats.maxHealth) {enemyText.text = enemy.stats.name + ' ' + hpList[10]}
+		else {enemyText.text = enemy.stats.name + ' ' + hpList[enemyHealthRounded]};
+
+}
+
 function update() {
 }
 
